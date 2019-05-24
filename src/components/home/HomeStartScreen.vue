4bc853aa340b3188a082d9627747415861d49685
Tharaka Devinda <tharaka@vista.aero>
2019-05-24 16:29:28 +0530

    added some content to home page

--- src/components/home/HomeStartScreen.vue
+++ src/components/home/HomeStartScreen.vue
@@ -18,8 +18,10 @@
                 </v-img>
             </v-flex> 
             <v-flex xs12 sm7 md8 lg8 xl8 class="">
-                <p class="google-font mb-1" style="font-weight: 350;color: #616161;font-size:300%"><b>Make good things 
-                    <span style="color: #1a73e8;">together</span>.</b>
+                <p class="google-font mb-1" style="font-weight: 350;color: #616161;font-size:300%">
+                    <b>by developers
+                    <span style="color: #1a73e8;"> | </span>for 
+                    <span style="color: #1a73e8;"> developers </span></b>
                 </p>
                 <p class="google-font mt-0" style="font-size:180%">{{chapterDetails.ChapterName}}</p>
                 <p class="google-font" style="font-size:110%">{{chapterDetails.ChapterShortDescription}}</p>
